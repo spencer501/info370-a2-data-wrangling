dc90134d909e841a1358de89080e3bfba81a3d96
Spencer <spencer.p.816@gmail.com>
2017-02-03 02:43:10 -0800

    Initial attempt at Gini index

--- metric_functions.R
+++ metric_functions.R
@@ -1,2 +1,21 @@
 # File containing functions for various segregation metrics
 #
+
+# Gini
+gini <- function(city) {
+
+    t <- city$pop
+    p <- city$pct.not.white
+    x <- city$pop.not.white
+
+
+    i.j <- expand.grid(1:nrow(city), 1:nrow(city))
+
+    topSum <- function(i, j) {
+        sum(t[i] * t[j] * abs(p[i] - p[j]))
+    }
+
+    index <- topSum(i.j[, 1], i.j[, 2]) / (2 * sum(t)**2 * (sum(x)/sum(t))*(1-(sum(x)/sum(t))))
+
+    return(index)
+}
