283579d262404abf4303068c38adaacedeee147a
Spencer <spencer.p.816@gmail.com>
2017-02-03 20:43:44 -0800

    Add geographical data to city data

--- analysis.R
+++ analysis.R
@@ -7,31 +7,7 @@ library(dplyr)
 # Get functions for segregation metrics
 source('metric_functions.R')
 
-# store all data as a list of dataframes
-# help from: http://stackoverflow.com/questions/5319839/,
-#            http://stackoverflow.com/questions/1105659/
-folder.location <- file.path('data/prepped')
-
-filenames <- list.files(
-    path = folder.location,
-    pattern = '*.csv')
-
-city.data <- lapply(
-    file.path(
-        folder.location,
-        filenames),
-    read.csv) %>%
-
-    setNames(
-        gsub('_race.csv',
-             '',
-             filenames,
-             fixed = TRUE))
-
-tmp.baltimore <- city.data$baltimore
-
-
-# Get geographical data
+# Get geographical data (to add to city data)
 geo.data <- read.delim('data/Gaz_tracts_national.txt') %>%
     select(GEOID,
            ALAND,
@@ -41,8 +17,40 @@ geo.data <- read.delim('data/Gaz_tracts_national.txt') %>%
            lat = INTPTLAT,
            long = INTPTLONG)
 
+# store all data as a list of dataframes
+# help from: http://stackoverflow.com/questions/5319839/,
+#            http://stackoverflow.com/questions/1105659/
+folder.location <- file.path('data/prepped')
 
+filenames <- list.files(path = folder.location,
+                        pattern = '*.csv')
 
+city.data <- lapply(file.path(folder.location,
+                              filenames),
+                    read.csv,
+                    stringsAsFactors = FALSE) %>%
 
+    # make list names nice
+    setNames(gsub('_race.csv',
+                  '',
+                  filenames,
+                  fixed = TRUE)) %>%
 
+    # Remove "-exclude" suffix from some GEOIDs
+    # and convert to double
+    lapply(function(df) {
+        df <- df %>%
+            mutate(GEOID = as.double(gsub('-exclude',
+                                          '',
+                                          GEOID,
+                                          fixed = TRUE)))
+        return(df)
+    }) %>%
 
+    # add geographical data
+    lapply(function(df) {
+        df <- df %>%
+            inner_join(geo.data,
+                       by = "GEOID")
+        return(df)
+    })
